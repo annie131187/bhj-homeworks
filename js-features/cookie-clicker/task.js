eb6d036c5b51bd4905c3e110572a7724498b8052
Anna Sheremetova <annie131187@gmail.com>
2022-12-09 17:53:37 +0700

    tasks2,3 modified

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,5 @@
 const cookie = document.getElementById('cookie');
 const clickerCounter = document.getElementById('clicker__counter');
 cookie.onclick = function() {
-    clickerCounter.textContent++;
-    if (clickerCounter.textContent % 2 === 0) {
-        cookie.width = 200;
-    } else {
-        cookie.width = 300;
-    }
+    cookie.width = ++clickerCounter.textContent % 2 ? 300 : 200;
 }
---
+++ js-features/mole-game/task.js
@@ -0,0 +1,20 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+const holes = Array.from(document.querySelectorAll('.hole'));
+
+holes.forEach(function(hole) {
+    hole.addEventListener('click', () => {
+        hole.classList.contains('hole_has-mole') ? dead.textContent++ : lost.textContent++;
+    });
+    if (dead.textContent === 10) {
+        alert('Победа!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+    } else if (lost.textContent === 5) {
+        alert('Вы проиграли!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+    }
+});
+
+
