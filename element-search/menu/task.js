2570d1cf1dc9b80d2deeddd7e43118fc9bb655e4
Anna Sheremetova <annie131187@gmail.com>
2022-12-16 14:22:58 +0700

    task 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,20 @@
 const links = [...document.querySelectorAll('.menu__link')];
+const subMenu = [...document.querySelectorAll('.menu_sub')];
 
+function closeSub (current = null) {
+    subMenu.forEach(item => {
+        if (item != current) item.classList.remove('menu_active');
+    });
+}
 
 links.forEach(link => {
     link.addEventListener('click', (event) => {
+        console.log(event);
         if (link.nextElementSibling) {
             event.preventDefault();
-            link.nextElementSibling.classList.toggle('menu_active');
+            closeSub(event.target.nextElementSibling);
+            event.target.nextElementSibling.classList.toggle('menu_active');
+            
         }
     });
 });
