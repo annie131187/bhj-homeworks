03cd66648cfc49aa0c43e3260141710c31212710
Anna Sheremetova <annie131187@gmail.com>
2023-01-05 15:46:50 +0700

    task 2 modified

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,19 +1,20 @@
 const rotator = document.querySelector('.rotator');
 const rotators = Array.from(document.querySelectorAll('.rotator__case'));
-let timeoutDelay = 0;
+let item = rotators[rotators.findIndex(item => item.classList.contains('rotator__case_active'))];
+item.style.color = item.dataset.color;
+let timeoutDelay = item.dataset.speed;
 
-let timeoutId = setTimeout(function rotate() {
+let timeoutId = setTimeout(function rotate(item) {
     item = rotators[rotators.findIndex(item => item.classList.contains('rotator__case_active'))];
-    item.style.color = item.dataset.color;
-    let delay = item.dataset.speed;
-    timeoutDelay = delay;
     if (item.nextElementSibling !== null) {
         item.classList.remove('rotator__case_active');
         item.nextElementSibling.classList.add('rotator__case_active');
         item.nextElementSibling.style.color = item.nextElementSibling.dataset.color;
+        timeoutDelay = item.nextElementSibling.dataset.speed;
     } else {
         item.classList.remove('rotator__case_active');
         item = rotator.firstElementChild.classList.add('rotator__case_active');
+        timeoutDelay = rotator.firstElementChild.dataset.speed;
     }
     timeoutId = setTimeout(rotate, timeoutDelay);
 }, timeoutDelay);
