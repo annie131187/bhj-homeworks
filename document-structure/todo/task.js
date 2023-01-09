27dbbf0f505938d6e4cd263caf80f81b02e900ea
Anna Sheremetova <annie131187@gmail.com>
2023-01-09 17:59:22 +0700

    task 1,2,3 modified

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,16 +4,21 @@ const input = tasksForm.querySelector('.tasks__input');
 
 tasksForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    
-    tasksList.innerHTML += `
-        <div class="task">
-            <div class="task__title">
-            ${input.value}
+
+    if (input.value.trim() == '') {
+        input.value = '';
+        return;
+    } else {
+        tasksList.insertAdjacentHTML("afterbegin", `
+            <div class="task">
+                <div class="task__title">
+                ${input.value.trim()}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
             </div>
-            <a href="#" class="task__remove">&times;</a>
-        </div>
-    `;
-    input.value = '';
+        `);
+        input.value = '';
+    }
     
     let current_tasks = document.querySelectorAll('.task__remove');
     for (let i = 0; i < current_tasks.length; i++) {
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,25 @@
 const tooltipLinks = [...document.querySelectorAll('.has-tooltip')];
 
 tooltipLinks.forEach(item => {
-    item.addEventListener('click', (event) => {
-        event.preventDefault();
-
+    item.addEventListener('click', evt => {
+        evt.preventDefault();
+        console.log(evt.type);
+        console.log(evt.currentTarget);
         removeTooltipActive();
-
-        item.innerHTML += `
+        
+        item.insertAdjacentHTML("afterend", `
             <div class="tooltip">
                 ${item.getAttribute('title')}
             </div>
-        `
+        `);
         let tooltip = document.querySelector('.tooltip');
-        tooltip.classList.toggle('tooltip_active');
+        tooltip.classList.add('tooltip_active');
+        tooltip.style.cssText = 'position: absolute;'
         
-        let coords = item.getBoundingClientRect();
+        let coords = getCoords(item);
         tooltip.style.left = coords.left + 'px';
-        tooltip.style.top = coords.top + item.offsetHeight + 'px';
+        tooltip.style.top = coords.bottom + 'px';
+        
     });
 });
 
@@ -25,7 +28,16 @@ function removeTooltipActive() {
     if (tooltip) {
         tooltip.classList.remove('tooltip_active');
         tooltip.remove(); 
-    } else {
-        return;
     }
 }
+
+function getCoords(item) {
+    let box = item.getBoundingClientRect();
+  
+    return {
+      top: box.top + window.pageYOffset,
+      right: box.right + window.pageXOffset,
+      bottom: box.bottom + window.pageYOffset,
+      left: box.left + window.pageXOffset
+    };
+  }
